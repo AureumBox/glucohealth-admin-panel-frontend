65847a9762112c3422cc1f1b4547fcc150238d14
Jose Andres <87492801+Noralgorithm@users.noreply.github.com>
2024-03-21 11:25:06 -0400

    feat: login

--- src/services/auth/login.ts
+++ src/services/auth/login.ts
@@ -2,13 +2,14 @@ import axios from 'axios';
 // Own
 import { API_BASE_URL } from 'config/constants';
 import BackendError from 'exceptions/backend-error';
+import { BackendResponse } from 'services/types';
 
 const URL = `${API_BASE_URL}/auth/login`;
 
-export default async function login(body: LoginBody): Promise<LoginResponse> {
+export default async function login(body: LoginBody): Promise<LoginResponseData> {
   try {
-    const response = await axios.post<LoginResponse>(URL, body);
-    return response.data;
+    const response = await axios.post<BackendResponse<LoginResponseData>>(URL, body);
+    return response.data.data;
   } catch (error: unknown) {
     throw new BackendError(error);
   }
@@ -19,8 +20,6 @@ export interface LoginBody {
   password: string;
 }
 
-export interface LoginResponse {
-  name: string;
-  email: string;
+export interface LoginResponseData {
   token: string;
 }

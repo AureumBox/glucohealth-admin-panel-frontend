2928eddd592f8ef0e5b31c6e5e2207ccd89e625b
Aurimart Garcia <aurimart.liliana@gmail.com>
2024-04-27 10:02:45 -0400

    fix to get-nurse service

--- src/services/nurses/get-nurse.ts
+++ src/services/nurses/get-nurse.ts
@@ -2,15 +2,17 @@ import axios from "axios";
 // Own
 import { API_BASE_URL } from "config/constants";
 import BackendError from "exceptions/backend-error";
+import addQueryParams from "services/add-query-params";
 import { BackendResponse } from "services/types";
 import store from "store";
 import { Nurse } from "types/nurse";
 
-const URL = `${API_BASE_URL}/nurses`;
+const URL = `${API_BASE_URL}/nurse`;
 
 export default async function getNurse(id: string): Promise<Nurse> {
   try {
-    const response = await axios.get<BackendResponse<Nurse>>(`${URL}/${id}`, {
+    const urlWithId = addQueryParams(URL, { id });
+    const response = await axios.get<BackendResponse<Nurse>>(urlWithId, {
       headers: {
         Authorization: `Bearer ${store.getState().auth.token}`,
       },
